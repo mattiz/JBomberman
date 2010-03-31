40448db0b0b96e5eab7c1f49f9b7d4d606537049
Mathias Bjerke <mathbje@gmail.com>
2010-03-31 22:53:37 +0200

    Added class Screenmanager to handle full screen and resolutions Test: edited project JDK to 1.6, is it working?

--- src/main/java/org/moogirc/jbomberman/App.java
+++ src/main/java/org/moogirc/jbomberman/App.java
@@ -1,13 +1,30 @@
 package org.moogirc.jbomberman;
 
+import javax.swing.*;
+import java.awt.*;
+
+
 /**
- * Hello world!
+ * Default entry point
  *
+ * @author Mathias Bjerke
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main( String[] args ) throws Exception {
+		ScreenManager sm = new ScreenManager();
+
+		DisplayMode modes[] = sm.getCompatibleDisplayModes();
+
+		for( DisplayMode mode : modes ) {
+			if( mode.getWidth() == 640 && mode.getHeight() == 480 ) {
+				sm.setFullScreen( mode );
+			}
+		}
+
+		JFrame window = sm.getWindow();
+
+		Graphics2D g2 = (Graphics2D) window.getGraphics();
+
+		g2.drawString( "Hello", 20, 20 );
+	}
 }
---
+++ src/main/java/org/moogirc/jbomberman/ScreenManager.java
@@ -0,0 +1,90 @@
+package org.moogirc.jbomberman;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+/**
+ * The class that manages all the screen activities. Examples are setting the resulution,
+ * setting full screen, returning to normal mode, and so on.
+ *
+ * @author Mathias Bjerke
+ */
+public class ScreenManager {
+	private GraphicsEnvironment environment;
+	private GraphicsDevice device;
+	private JFrame window;
+
+
+	/**
+	 * Default constructor which gets the local graphics environment and
+	 * default screen device
+	 */
+	public ScreenManager() {
+		environment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		device = environment.getDefaultScreenDevice();
+		window = new JFrame();
+	}
+
+
+	/**
+	 * Get all the compatible display modes on the system
+	 * @return an array of compatible display modes
+	 */
+	public DisplayMode[] getCompatibleDisplayModes() {
+		return device.getDisplayModes();
+	}
+
+
+	/**
+	 * Get the current display mode
+	 * @return the current display mode
+	 */
+	public DisplayMode getCurrentDisplayMode() {
+		return device.getDisplayMode();
+	}
+
+
+	/**
+	 * Set the window in full screen
+	 * @param displayMode which displaymode to set the window
+	 */
+	public void setFullScreen( DisplayMode displayMode ) {
+		window.setUndecorated( true );
+		window.setResizable( false );
+
+		device.setFullScreenWindow( window );
+
+		if( displayMode != null && device.isDisplayChangeSupported() ) {
+			try {
+				device.setDisplayMode(displayMode);
+				
+			} catch (IllegalArgumentException ex) {
+				// ignore - illegal mode for this device
+			}
+		}
+	}
+
+
+	/**
+	 * Restore the regular windowed screen
+	 */
+	public void restoreScreen() {
+		Window window = device.getFullScreenWindow();
+
+		if( window != null ) {
+			window.dispose();
+		}
+
+		device.setFullScreenWindow( null );
+	}
+
+
+	/**
+	 * Get the window object
+	 * @return JFrame
+	 */
+	public JFrame getWindow() {
+		return window;
+	}
+}
