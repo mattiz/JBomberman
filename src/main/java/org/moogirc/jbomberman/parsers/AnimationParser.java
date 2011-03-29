3cdabdae9edafae8126c2f0f09293b7ae6ef7169
Simen <simen@PhoneBone.(none)>
2011-03-29 19:22:57 +0200

    Added a class that will parse the original Atomic Bomberman .ani files

---
+++ src/main/java/org/moogirc/jbomberman/parsers/AnimationParser.java
@@ -0,0 +1,10 @@
+package org.moogirc.jbomberman.parsers;
+
+import java.io.File;
+import org.newdawn.slick.Image;
+
+public class AnimationParser {
+	public static Image readAnimationFile(File animationFile) {
+		return null;
+	}
+}
