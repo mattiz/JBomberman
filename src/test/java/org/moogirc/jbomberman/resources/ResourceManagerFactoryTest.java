00d537115b4a0e9cd3782afb8413bc812bb86373
Mathias Bjerke <mathbje@gmail.com>
2011-04-02 19:47:49 +0200

    * Added new test

---
+++ src/test/java/org/moogirc/jbomberman/resources/ResourceManagerFactoryTest.java
@@ -0,0 +1,33 @@
+package org.moogirc.jbomberman.resources;
+
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+
+/**
+ * Tests for the class ResourceManagerFactory
+ *
+ * @author Mathias Bjerke <mathias@verida.no>
+ */
+public class ResourceManagerFactoryTest {
+	private ResourceManagerFactory resourceManagerFactory;
+
+
+	@Before
+	public void setUp() {
+		resourceManagerFactory = new ResourceManagerFactory();
+	}
+
+
+	@Test
+	public void testGetThemes() {
+
+	}
+
+
+	@Test
+	public void testCreateResourceManager() {
+
+	}
+}
